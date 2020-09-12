07790a6253442467cbacd35f8e18950dede8777e
Tony Park <tonypark0403@gmail.com>
2020-09-12 00:35:02 -0400

    added function for picture in picture

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch Error Here
+    console.log('whoops, error here:', error);
+  }
+}
+
+button.addEventListener('click', async () => {
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
